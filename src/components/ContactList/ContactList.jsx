098e92f6eac58606e40fcb84706a49c936759cd6
Artem Hordiyevskyy <artem2500@gmail.com>
2024-03-09 23:31:04 +0200

    rewrite ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,22 +3,15 @@ import css from "./ContactList.module.css";
 import { useSelector } from "react-redux";
 import { selectContacts, selectNameFilter } from "../../redux/selectors";
 
-function filterContacts(contacts, filter) {
-  const filteredContacts = [];
-  contacts.map((contact) => {
-    if (contact.name.toUpperCase().includes(filter.toUpperCase())) {
-      filteredContacts.push(contact);
-    }
-  });
-  return filteredContacts;
-}
-
 export default function ContactList() {
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectNameFilter);
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toUpperCase().includes(filter.toUpperCase())
+  );
   return (
     <ul className={css.contactList}>
-      {filterContacts(contacts, filter).map((contact) => {
+      {filteredContacts.map((contact) => {
         return <Contact key={contact.id} contact={contact} />;
       })}
     </ul>
